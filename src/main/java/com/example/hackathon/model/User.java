bf5f8bb782f4944284e4f90c7892b89209f392b9
Juraj Vetrak <juraj.vetrak@student.tuke.sk>
2022-12-03 19:13:05 +0100

    frontend

--- src/main/java/com/example/hackathon/model/User.java
+++ src/main/java/com/example/hackathon/model/User.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "User")
-public class User {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
